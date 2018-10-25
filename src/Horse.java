02752b68dacb816e85402a12ea50097932b214fa
BT_1N3_09@bktech.local <BT_1N3_09@bktech.local>
2018-10-25 09:11:39 -0400

    Initial commit

---
+++ src/Horse.java
@@ -0,0 +1,27 @@
+public class Horse {
+
+    String name;
+
+    int weight;
+
+    static Horse[] spaces;
+
+    public Horse(String string, int i) {
+
+        name = string;
+
+        weight = i;
+
+    }
+
+    public Object getName() {
+
+        return name;
+    }
+
+    public Object getWeight() {
+
+        return weight;
+    }
+
+}
